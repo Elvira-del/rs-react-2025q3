fe6ffb93feea5d6fec4d36eec336d2f36cfddcbf
Elvira Krutova <ninelostr@gmail.com>
2025-07-27 23:58:58 +0100

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ErrorBoundary from './components/error/ErrorBoundary/ErrorBoundary.tsx';
@@ -5,11 +6,16 @@ import { FallbackUI } from './components/error/FallbackUI/FallbackUI.tsx';
 import App from './App.tsx';
 import './index.css';
 
+const router = createBrowserRouter(
+  [{ path: '/', element: <App />, errorElement: <FallbackUI /> }],
+  { basename: '/rs-react-2025q3' }
+);
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary fallback={<FallbackUI />}>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </StrictMode>
 );
