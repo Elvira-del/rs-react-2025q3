e1d03dfed29e47c990190aa49ca41025edf68450
Elvira Krutova <ninelostr@gmail.com>
2025-08-04 00:42:52 +0100

    test: add tests for flyout

---
+++ src/pages/home/components/SelectedFlyout/__tests__/SelectedFlyout.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { useStore } from '../../../../../app/store';
+import { SelectedFlyout } from '../SelectedFlyout';
+import userEvent from '@testing-library/user-event';
+
+beforeEach(() => {
+  useStore.setState({ selectedItems: [] });
+});
+
+describe('Flyout', () => {
+  test('does not render if no items are selected', () => {
+    const { queryByText } = render(<SelectedFlyout />);
+
+    expect(queryByText(/selected/)).toBeNull();
+  });
+
+  test('shows correct count when items are selected', () => {
+    useStore.setState({
+      selectedItems: [
+        { id: 1, name: 'Rick' },
+        { id: 2, name: 'Morty' },
+      ],
+    });
+    const { getByText } = render(<SelectedFlyout />);
+    expect(getByText('2 items selected')).toBeInTheDocument();
+  });
+
+  test('clicking "Unselect all" calls removeAllSelectedItems', async () => {
+    const user = userEvent.setup();
+    const removeAllSelectedItems = vi.fn();
+    useStore.setState({
+      selectedItems: [{ id: 1, name: 'Rick' }],
+      removeAllSelectedItems,
+    });
+    const { getByRole } = render(<SelectedFlyout />);
+    const unselectButton = getByRole('button', { name: /unselect all/i });
+    await user.click(unselectButton);
+    expect(removeAllSelectedItems).toHaveBeenCalled();
+  });
+});
